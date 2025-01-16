d98bb70402533989fca2ad4af95b37cd9162178a
kriwnoo <s6652410019@sau.ac.th>
2025-01-16 17:14:33 +0700

    v.4 messi

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:dti_sau_project/views/login_ui.dart';
 import 'package:dti_sau_project/views/welcome_ui.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +23,7 @@ class _DtiSauState extends State<DtiSau> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeUi(),
+      home: LoginUi(),
     );
   }
 }
